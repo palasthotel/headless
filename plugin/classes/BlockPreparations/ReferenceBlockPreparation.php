dd3b5bb7927e25b2aae9f5fd16e80685aa9ac8b1
EdwardBock <edward.bock@palasthotel.de>
2022-07-06 14:44:32 +0200

    reference block preparation

---
+++ plugin/classes/BlockPreparations/ReferenceBlockPreparation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Palasthotel\WordPress\Headless\BlockPreparations;
+
+use Palasthotel\WordPress\Headless\Interfaces\IBlockPreparation;
+use Palasthotel\WordPress\Headless\Model\BlockName;
+
+class ReferenceBlockPreparation implements IBlockPreparation {
+
+	function blockName(): ?BlockName {
+		return new BlockName("core", "block");
+	}
+
+	function prepare( array $block ): array {
+
+		if(!empty($block["attrs"]) && !empty($block["attrs"]["ref"])){
+			$post = get_post($block["attrs"]["ref"]);
+			if($post instanceof \WP_Post && $post->post_type == 'wp_block'){
+				$blocks = parse_blocks($post->post_content);
+				if(count($blocks) == 1){
+					$theBlock = $blocks[0];
+					$theBlock["ref"] = $post->ID;
+					return $theBlock;
+				}
+			}
+
+		}
+
+		return $block;
+	}
+}
--- plugin/classes/Extensions.php
+++ plugin/classes/Extensions.php
@@ -6,6 +6,8 @@ use Palasthotel\WordPress\Headless\BlockPreparations\FreeFormBlockPreparation;
 use Palasthotel\WordPress\Headless\BlockPreparations\GalleryBlockPreparation;
 use Palasthotel\WordPress\Headless\BlockPreparations\ImageBlockPreparation;
 use Palasthotel\WordPress\Headless\BlockPreparations\MoreBlockPreparation;
+use Palasthotel\WordPress\Headless\BlockPreparations\ParagraphBlockPreparation;
+use Palasthotel\WordPress\Headless\BlockPreparations\ReferenceBlockPreparation;
 use Palasthotel\WordPress\Headless\Components\Component;
 use Palasthotel\WordPress\Headless\Extensions\ContentAttachments;
 use Palasthotel\WordPress\Headless\Extensions\ContentBlocks;
@@ -32,13 +34,20 @@ class Extensions extends Component {
 		$this->postRouteExtensions = new PostRouteExtensions();
 		$this->blockPreparations   = new BlockPreparations();
 
+		add_action( Plugin::ACTION_REGISTER_BLOCK_PREPARATION_EXTENSIONS, [ $this, 'block_preparation_extensions_with_prio' ], 2 );
 		add_action( Plugin::ACTION_REGISTER_BLOCK_PREPARATION_EXTENSIONS, [ $this, 'block_preparation_extensions' ] );
 		add_action( Plugin::ACTION_REGISTER_POST_ROUTE_EXTENSIONS, [ $this, 'post_route_extensions' ] );
 
 		add_action( 'rest_api_init', [ $this, 'rest_api_init' ] );
 	}
 
+	public function block_preparation_extensions_with_prio(BlockPreparations $extensions){
+		// needs to be the very first preparation step so others can apply to the result
+		$extensions->add(new ReferenceBlockPreparation());
+	}
+
 	public function block_preparation_extensions( BlockPreparations $extensions ) {
+		$extensions->add( new ParagraphBlockPreparation() );
 		$extensions->add( new MoreBlockPreparation() );
 		$extensions->add( new ImageBlockPreparation() );
 		$extensions->add( new GalleryBlockPreparation() );
