096c862bed2457346e47f8203cabdbf69ea10c22
EdwardBock <edward.bock@palasthotel.de>
2023-01-30 07:11:00 +0100

    transpiled

--- plugin/dist/gutenberg.ts.asset.php
+++ plugin/dist/gutenberg.ts.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '4bd51b78e35e5e87db39');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => 'bbfc4515a2bb626b7b7c');
