84418300fca2fbe440e139a5e11665172fc5bc19
Edward <edward.bock@palasthotel.de>
2024-06-03 19:12:06 +0200

    fix coerce

--- src/schema/menus.ts
+++ src/schema/menus.ts
@@ -1,9 +1,8 @@
 import {z} from "zod";
-import {authorIdSchema, postIdSchema} from "@palasthotel/wp-rest";
 
 export const menuItemResponseSchema = z.object({
-        ID: postIdSchema,
-        post_author: authorIdSchema,
+        ID: z.coerce.number(),
+        post_author: z.coerce.number(),
         post_date: z.string(),
         post_date_gmt: z.string(),
         post_content: z.string(),
@@ -19,14 +18,14 @@ export const menuItemResponseSchema = z.object({
         post_modified: z.string(),
         post_modified_gmt: z.string(),
         post_content_filtered: z.string(),
-        post_parent: z.number(),
+        post_parent: z.coerce.number(),
         guid: z.string(),
-        menu_order: z.number(),
+        menu_order: z.coerce.number(),
         post_type: z.string(),
         post_mime_type: z.string(),
         comment_count: z.string(),
         filter: z.string(),
-        db_id: z.number(),
+        db_id: z.coerce.number(),
         menu_item_parent: z.string(),
         object_id: z.number(),
         object: z.string(),
