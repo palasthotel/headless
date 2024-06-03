f9a70f5f8f9e094328ca58bce8ce3af0c6be4d4a
Edward <edward.bock@palasthotel.de>
2024-06-03 19:04:50 +0200

    fix menu schema

--- src/util/hierarchy.ts
+++ src/util/hierarchy.ts
@@ -1,10 +1,10 @@
 import {buildHierarchy, Hierarchy} from "@palasthotel/wp-rest";
-import {MenuResponse} from "../@types";
+import {MenuItemResponse, MenuResponse} from "../@types";
 
 export function menuAsHierarchy(
-    menu: MenuResponse[],
-): Hierarchy<MenuResponse>[] {
-    return buildHierarchy<MenuResponse>(
+    menu: MenuResponse,
+): Hierarchy<MenuItemResponse>[] {
+    return buildHierarchy<MenuItemResponse>(
         menu,
         (item) => item.ID,
         (item) => item.menu_item_parent === '0' ? false : parseInt(item.menu_item_parent),
