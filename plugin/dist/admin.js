612f7c59657f066d681e25033eee6b65bc7753a5
JanaME <jana.eggebrecht@palasthotel.de>
2026-03-27 10:26:56 +0100

    remove plugin/dist from version control

--- plugin/dist/admin.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";window.addEventListener("DOMContentLoaded",(async()=>{window.HeadlessAdmin.preview_is_active&&await Promise.all(window.HeadlessAdmin.frontends.map((e=>e+window.HeadlessAdmin.preview_path)).map((e=>fetch(e,{credentials:"include"}).then((e=>e.json())))))}))})();
